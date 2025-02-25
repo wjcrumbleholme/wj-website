89df619e859f212296733e867edc970082d176ff
wjcrumbleholme <64094813+wjcrumbleholme@users.noreply.github.com>
2025-02-25 00:10:07 +0000

    Clickable projects + ability to add more

--- app.js
+++ app.js
@@ -19,3 +19,28 @@ prev.addEventListener("click", ()=> {
 next.addEventListener("click", ()=> {
     list.scrollLeft += (itemWidth + padding);
 })
+
+fetch("projects/assets/projects.json")
+    .then(response => response.json())
+    .then(projects => {
+        projects.forEach(project => {
+            const projectDiv = document.createElement("div");
+            projectDiv.classList.add("scroll__item");
+
+            projectDiv.innerHTML = `
+                <div class="scroll__item-img">
+                    <img src="${project.image}" alt="PLACEHOLDER">
+                </div>
+                <div class="scroll__item-title">
+                <h2>${project.title}</h2>
+                </div>
+                <div class="scroll__item-desc">
+                <p>${project.description}</p>
+                <a href="${project.link}" class="fill-div"></a>
+                </div>
+            `;
+
+            list.appendChild(projectDiv);
+        });
+    })
+    .catch(error => console.error("Error loading projects: ", error));
